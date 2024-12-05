e5a76d8058688bfeda601c01eb1a44dd68b8a11e
Terry Dutton <terrydutton@Terrys-MacBook-Air.local>
2024-12-05 06:23:24 +0000

    Swapped dict arrays for sets

--- Day 05/part2.ts
+++ Day 05/part2.ts
@@ -11,16 +11,16 @@ const porList = toNumberLists(por, '|');
 const toPrint = toNumberLists(p2p, ',');
 
 const porDict = porList.reduce((acc, [isBefore, isAfter]) => {
-    if (!acc[isBefore]) acc[isBefore] = [];
-    acc[isBefore].push(isAfter);
+    if (!acc[isBefore]) acc[isBefore] = new Set();
+    acc[isBefore].add(isAfter);
     return acc;
-}, {} as { [key: number]: number[] });
+}, {} as { [key: number]: Set<number> });
 
 const sortIntoPrintOrder = (p1: number, p2: number) => {
     const pagesAfterP1 = porDict[p1];
     const pagesAfterP2 = porDict[p2];
-    if (pagesAfterP1.includes(p2)) return -1;
-    if (pagesAfterP2.includes(p1)) return 1;
+    if (pagesAfterP1.has(p2)) return -1;
+    if (pagesAfterP2.has(p1)) return 1;
     return 0;
 };
 
